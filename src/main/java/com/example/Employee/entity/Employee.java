9ad3a842ec19e44d89e48711585fa7f8611b83e6
theeunknownn <atharvajadhav6901@gmail.com>
2022-11-09 20:07:36 +0530

    Update The classes

--- src/main/java/com/example/Employee/entity/Employee.java
+++ src/main/java/com/example/Employee/entity/Employee.java
@@ -1,4 +1,61 @@
 package com.example.Employee.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public Employee(){
+
+    }
+    public Employee(int id, String firstname, String lastname, String email){
+        super();
+        this.id=id;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.email=email;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/Employee/service/EmployeeService.java
+++ src/main/java/com/example/Employee/service/EmployeeService.java
@@ -1,4 +1,52 @@
 package com.example.Employee.service;
 
+
+import com.example.Employee.entity.Employee;
+import com.example.Employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repo;
+
+    //get all employees
+    public List<Employee> getAll(){
+
+        //find all employees data and return it
+        return repo.findAll();
+    }
+
+    //save Employee data
+    public Employee add(Employee emp){
+        // save the data to the employee
+        return repo.save(emp);
+    }
+
+    //get the employee date by id
+    public Employee getById(int id){
+        Employee emp = repo.findById(id).get();
+        //if the employee not found throw error
+        if (emp==null){
+            throw new RuntimeException("Employee not found");
+        }
+        return emp;
+    }
+
+    //delete employee by id
+    public String deleteById(int id){
+        Employee emp = repo.findById(id).get();
+        if (emp==null){
+            throw new RuntimeException("Employee not found");
+        }
+        repo.deleteById(id);
+        return "Deleted:"+emp.getFirstname()+" "+emp.getLastname();
+    }
+
+
+
 }
